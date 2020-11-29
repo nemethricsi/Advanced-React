29111a98edb4ea19d442ff758de24be9bfcace64
Richárd Németh <richard.nemeth@futurevending.hu>
2020-11-29 22:22:36 +0100

    add updateItem logic to GraphQL

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   // forward simple GET request from Yoga to Prisma:
   items: forwardTo("db"),
+  item: forwardTo("db"),
 
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
