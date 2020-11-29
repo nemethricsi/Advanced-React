17ea2dc8ec91337ae7280d999e5766e21bca1884
Richárd Németh <richard.nemeth@futurevending.hu>
2020-11-29 14:03:15 +0100

    add Item model and createItem mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,20 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // title: args.title,
+          // description: args.description,
+          ...args,
+        },
+      },
+      info
+    );
+
+    return item;
+  },
+};
 
 module.exports = Mutations;
