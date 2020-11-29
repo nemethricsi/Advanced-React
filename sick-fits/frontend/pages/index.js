dd5edbbe0e4ef72c267d8e766c18c75caf07e1e1
Richárd Németh <richard.nemeth@futurevending.hu>
2020-11-29 20:37:45 +0100

    add Item and Items components

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,11 +1,8 @@
-import Link from "next/link";
+import Items from "../components/Items";
 
 const Home = () => (
   <div>
-    <p>Yo!</p>
-    <Link href='/sell'>
-      <a>Sell</a>
-    </Link>
+    <Items />
   </div>
 );
 
