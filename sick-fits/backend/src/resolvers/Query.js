97c75b1c003b4e08982906498c9de7d09f1206c8
Richárd Németh <richard.nemeth@futurevending.hu>
2020-11-30 00:54:53 +0100

    itemsConnection for Pagination

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,6 +4,7 @@ const Query = {
   // forward simple GET request from Yoga to Prisma:
   items: forwardTo("db"),
   item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
 
   // async items(parent, args, ctx, info) {
   //   const items = await ctx.db.query.items();
