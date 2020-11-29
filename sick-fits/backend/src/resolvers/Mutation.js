5efdbbcbe8c41c7c2349f2b45709a4fc487c1aba
Richárd Németh <richard.nemeth@futurevending.hu>
2020-11-29 23:34:34 +0100

    add deleteItem(backend)

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -31,6 +31,14 @@ const Mutations = {
       info
     );
   },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // 2. Check if they own the item or have the permissions
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
