ebe64d0930958d4410bbf2f49309cff60ede8ab4
Richárd Németh <richard.nemeth@futurevending.hu>
2020-12-06 21:28:19 +0100

    set frontend back to normal

--- sick-fits/frontend/config.js
+++ sick-fits/frontend/config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://sickfits-yoga-prod-nr.herokuapp.com/`;
+export const prodEndpoint = `https://sickfits-yoga-prod-nr.herokuapp.com`;
 export const perPage = 4;
--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -6,12 +6,12 @@ import { LOCAL_STATE_QUERY } from "../components/Cart";
 function createClient({ headers }) {
   return new ApolloClient({
     // connectToDevTools: true,
-    uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
+    uri: process.env.NODE_ENV === "development" ? endpoint : endpoint,
     request: (operation) => {
       operation.setContext({
-        // fetchOptions: {
-        //   credentials: "include",
-        // },
+        fetchOptions: {
+          credentials: "include",
+        },
         headers,
       });
     },
